1e30906baf5b7053e9248f2073af5046241ff08e
dhmin5693 <dhmin5693@naver.com>
2021-04-21 01:36:55 +0900

    project initialize

---
+++ src/test/java/min/study/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package min.study.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
